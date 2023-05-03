32fb0699593c31613d665900cc7e0e06aa6d44ba
MohamedAbobakr-7 <mohamedabobakr070@gmail.com>
2023-05-03 22:47:55 +0300

    Add files via upload

---
+++ 7/CV/CV.js
@@ -0,0 +1,9 @@
+// const container = document.querySelector('.c');
+
+// container.addEventListener('mouseenter', () => {
+//   container.querySelector('.nav2').style.display = 'block';
+// });
+
+// container.addEventListener('mouseleave', () => {
+//   container.querySelector('.nav2').style.display = 'none';
+// });
